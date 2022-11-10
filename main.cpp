5342ba5bf61a295bda9f343cafaeaf5e724f3131
farinaema <117036663+farinaema@users.noreply.github.com>
2022-11-10 15:36:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main(){
-    float a, b, c; // dichiarazione delle variabili
+    float a = 0, b = 0, c = 0; // dichiarazione delle variabili
     cin >> a >> b >> c; // assegnazione valori
     cout << (a*b)/2 << endl; //area triangolo
     cout << a*a << endl; //area quadrato
     cout << a*b << endl; // area rettangolo
     cout << ((a+b)*c)/2 << endl; // area trapezio
+    return 0
 }
